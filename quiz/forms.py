fd524eb6af90df7b21237b97f768ed0b169d66a8
hoangthanhlong <hoangthanhlong@gmail.com>
2016-07-28 22:07:30 +0700

    complete multiple quiz form

--- quiz/forms.py
+++ quiz/forms.py
@@ -17,19 +17,3 @@ class QuizForm(Form):
                                  for choice in choices_list]
         self.choices.name = 'quiz_{}'.format(kwargs['qid'])
 
-
-class MultiQuizForm(Form):
-    choices = []
-    submit = SubmitField('Submit Answer')
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.quiz_list = Quiz.query.filter(Quiz.id.in_(kwargs['qids'])).all()
-        for quiz in self.quiz_list:
-            self.choices.append(SelectMultipleField('Choices', coerce=int, choices=[],
-                                          widget=widgets.ListWidget(prefix_label=False),
-                                          option_widget=widgets.CheckboxInput()))
-            for choices in self.choices:
-                choices.choices.extend([(choice.id, choice.content)
-                                         for choice in quiz.choices])
-                # choices.name = 'quiz_{}'.format(quiz.id)
--- quiz/views.py
+++ quiz/views.py
@@ -1,9 +1,10 @@
 from flask import render_template, request, redirect, url_for
-from wtforms import RadioField
+from flask_wtf import Form
+from wtforms import SelectMultipleField, SubmitField, widgets
 
 from quiz import app
 from quiz.models import Quiz, Choice
-from quiz.forms import QuizForm, MultiQuizForm
+from quiz.forms import QuizForm
 
 
 def check_answer(qid, chosen):
@@ -24,10 +25,9 @@ def view_quiz(qid):
     form = QuizForm(request.form, qid=qid)
     result = ''
     if form.validate_on_submit():
-        chosen = request.form.getlist('quiz_{}'.format(qid))
-        chosen = [int(i) for i in chosen]
+        chosen = [int(i) for i in request.form.getlist('quiz_{}'.format(qid))]
         result = check_answer(qid, chosen)
-        return render_template('quiz.html', quiz=quiz, form=form, result=result)
+        #return render_template('quiz.html', quiz=quiz, form=form, result=result)
 
     return render_template('quiz.html', quiz=quiz, form=form, result=result)
 
@@ -36,7 +36,34 @@ def view_quiz(qid):
 @app.route('/test', methods=['GET', 'POST'])
 def test():
     qids = [1, 4, 9, 14, 17, 18, 23, 24, 27, 28, 34, 35, 36, 38, 39, 40, 43, 54, 55, 56, 59, 61, 62, 63, 65]
-    form = MultiQuizForm(request.form, qids=qids)
-    return render_template('multiquiz.html', qids=qids)
-    return render_template('multiquiz.html', form=form)
+    quizzes = Quiz.query.filter(Quiz.id.in_(qids)).all()
+
+
+    class MultiQuizForm(Form):
+        pass
+
+    for quiz in quizzes:
+        choices = [(choice.id, choice.content) for choice in quiz.choices]
+        field_name = 'quiz_{}'.format(quiz.id)
+        field = SelectMultipleField(field_name, coerce=int, choices=choices,
+                                    widget=widgets.ListWidget(prefix_label=False),
+                                    option_widget=widgets.CheckboxInput())
+        setattr(MultiQuizForm, field_name, field)
+
+
+    form = MultiQuizForm(request.form, quizzes=quizzes)
+
+    quiz_list = []
+
+    for quiz in quizzes:
+        quiz_list.append({'quiz': quiz,
+                          'choices': getattr(form, 'quiz_{}'.format(quiz.id))})
+
+    results = []
+    if form.validate_on_submit():
+        for qid in qids:
+            chosen = [int(i) for i in request.form.getlist('quiz_{}'.format(qid))]
+            results.append((qid, check_answer(qid, chosen)))
+    return render_template('multiquiz.html', form=form,
+                           quiz_list=quiz_list, results=results)
 
