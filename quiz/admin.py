8a41daaaa22d47e8b5d6d9b201c0305f571e51ee
hoangthanhlong <hoangthanhlong@gmail.com>
2016-07-31 21:46:13 +0700

    edit admin.py

--- quiz/admin.py
+++ quiz/admin.py
@@ -5,19 +5,8 @@ from quiz import admin, db, codemirror
 from quiz.models import Quiz, Choice
 
 
-class CodeMirrorPythonField(CodeMirrorField):
-#    widget = CodeMirrorWidget(language='python', config = {'lineNumbers': 'true'})
-    pass
-
 class QuizAdmin(sqla.ModelView):
-#    code = CodeMirrorField(language='python', config = {'lineNumbers': 'true'})
-#    form_overrides = dict(code=CodeMirrorPythonField)
-#    form_overrides = dict(code=CodeMirrorField(widget=CodeMirrorWidget(
-#        language='python', config={'lineNumbers': 'true'})))
-#    form_overrides = dict(code=CodeMirrorField(widget=CodeMirrorWidget(language='python')))
-
     inline_models = (Choice,)
-    pass
 
 
 class ChoiceAdmin(sqla.ModelView):
