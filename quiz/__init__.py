96506b77d579b31a7ed8faa20e77246e8f76c28f
hoangthanhlong <hoangthanhlong@gmail.com>
2016-07-31 21:48:38 +0700

    aedit init, template

--- quiz/__init__.py
+++ quiz/__init__.py
@@ -3,7 +3,6 @@ import os
 from flask import Flask
 from flask_admin import Admin
 from flask_bootstrap import Bootstrap
-# from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_codemirror import CodeMirror
 import jinja2_highlight
@@ -17,18 +16,15 @@ bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 admin = Admin(app, name='quiz', template_mode='bootstrap3')
 codemirror = CodeMirror(app)
-# login_manager = LoginManager()
-# login_manager.init_app(app)
 
 
 
-import quiz.models
+from quiz.models import import_db
 import quiz.views
+
 
 if not os.path.exists(os.path.join(app.config['BASE_DIR'], 'app.db')):
     db.create_all()
-    quiz.models.import_db()
+    import_db()
 
-# from flask_admin.contrib.sqla import ModelView
-# from quiz.admin import QuizAdmin
 import quiz.admin
