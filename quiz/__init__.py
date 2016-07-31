922a70c8f922b2cb41691e83e4b91fe4c8b63ae8
hoangthanhlong <hoangthanhlong@gmail.com>
2016-07-31 22:48:24 +0700

    app_context, pep8 code

--- quiz/__init__.py
+++ quiz/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, current_app
 from flask_admin import Admin
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
@@ -9,21 +9,21 @@ import jinja2_highlight
 
 
 app = Flask(__name__)
-app.jinja_options = dict(Flask.jinja_options)
-app.jinja_options.setdefault('extensions', []).append('jinja2_highlight.HighlightExtension')
-app.config.from_object('config')
-bootstrap = Bootstrap(app)
-db = SQLAlchemy(app)
-admin = Admin(app, name='quiz', template_mode='bootstrap3')
-codemirror = CodeMirror(app)
+with app.app_context():
+    current_app.jinja_options = dict(Flask.jinja_options)
+    current_app.jinja_options.setdefault('extensions', [])
+    current_app.jinja_options['extensions'].append('jinja2_highlight.HighlightExtension')
+    current_app.config.from_object('config')
+    bootstrap = Bootstrap(current_app)
+    db = SQLAlchemy(current_app)
+    admin = Admin(current_app, name='quiz', template_mode='bootstrap3')
+    codemirror = CodeMirror(current_app)
 
+    from quiz.models import import_db
+    import quiz.views
 
-from quiz.models import import_db
-import quiz.views
+    if not os.path.exists(os.path.join(app.config['BASE_DIR'], 'app.db')):
+        db.create_all()
+        import_db()
 
-
-if not os.path.exists(os.path.join(app.config['BASE_DIR'], 'app.db')):
-    db.create_all()
-    import_db()
-
-import quiz.admin
+    import quiz.admin
